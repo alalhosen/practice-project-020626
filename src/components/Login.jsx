243ebc1678b9feb7d92d896ab71409227c155016
Alal Hosen <alalhosen1992620120@gmail.com>
2026-06-16 23:58:51 +0600

    register function add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   const handleLogin = (e) => {
     e.preventDefault();
@@ -8,14 +10,10 @@ const Login = () => {
 
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+      <div className="hero-content flex-col">
+        <div className="text-center">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+        
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
@@ -51,6 +49,8 @@ const Login = () => {
                 <button className="btn btn-primary">Login</button>
               </div>
             </form>
+            <p>New to here! Please<Link to="/register"><button className="btn btn-link">Register</button>
+            </Link></p>
           </div>
         </div>
       </div>
