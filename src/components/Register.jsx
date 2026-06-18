88d24059e6a0fe73892120d713f9577a3317c1fb
Alal Hosen <alalhosen1992620120@gmail.com>
2026-06-18 23:40:37 +0600

    useCOntext use

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,21 +1,22 @@
-const Login = () => {
-  const handleLogin = (e) => {
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
   };
-const Register = () => {
     return (
        <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col">
         <div className="text-center">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Register now!</h1>
         
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <form onSubmit={handleLogin}>
+            <form onSubmit={handleRegister}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -44,10 +45,10 @@ const Register = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary">Register</button>
               </div>
             </form>
-            <p>New to here! Please<Link to="/register"><button className="btn btn-link">Register</button>
+            <p>New to here! Please<Link to="/login"><button className="btn btn-link">Login</button>
             </Link></p>
           </div>
         </div>
